7831145bdaa81bfd1c36b1ed6cb23e877a1fedbc
mihaituta <tutamihai@gmail.com>
2024-02-15 01:17:34 +0200

    Added simple enemy

--- World/Camera.gd
+++ World/Camera.gd
@@ -4,7 +4,3 @@ func _ready():
 	Events.room_entered.connect(func(room):
 		global_position = room.global_position;
 	)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
